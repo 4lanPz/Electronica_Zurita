ce77f8d874b05ea97050b1c377a04f293bc49388
Alan <alanstvn420@gmail.com>
2024-05-09 14:25:06 -0500

    logo

--- src/paginas/Forgot.jsx
+++ src/paginas/Forgot.jsx
@@ -29,77 +29,78 @@ export const Forgot = () => {
 
   return (
     <>
-      <div className="absolute inset-0 bg-[url('/images/tecnico.jpg')] bg-no-repeat bg-cover bg-center opacity-70 blur-sm"></div>
-      <div className="absolute inset-0 flex justify-center items-center">
-        <div className="w-auto lg:w-2/5 white  p-10 rounded-xl">
-          <div>
-            
-            <div className="flex items-center justify-center mb-5">
-              {/* Imagen */}
-              <img
-                src="/images/logopag.jpg"
-                alt="Logo"
-                className="w-40 h-auto mr-3"
-              />
-
-              {/* Texto */}
-              <div className="text-center">
-                <h1 className="poppins-bold uppercase text-black">
-                  !Olvidé mi
-                </h1>
-                <h1 className="poppins-bold uppercase text-black">
-                  contraseña¡
-                </h1>
-              </div>
-            </div>
-            <div className="text-center">
-              <small className="poppins-regular text-black block mt-4 ">
-                !Tranquilo técnico!
-              </small>
-              <small className="poppins-regular text-black block mb-4 ">
-                Le ayudamos a recuperar su contraseña
-              </small>
-            </div>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-1">
-                <label className="mb-1 block poppins-semibold">
-                  Correo electrónico
-                </label>
-                <input
-                  type="email"
-                  placeholder="Ingrese su correo electrónico"
-                  className="poppins-regular block w-full rounded-xl border border-gray-300 focus:border-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-700 py-1 px-2 text-black"
-                  name="email"
-                  onChange={handleChange}
+      <div className="w-full h-screen relative">
+        <div className="absolute inset-0 bg-[url('/images/tecnico.jpg')] bg-no-repeat bg-cover bg-center opacity-70 blur-sm"></div>
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div className="w-auto lg:w-2/5 white  p-10 rounded-xl">
+            <div>
+              <div className="flex items-center justify-center mb-5">
+                {/* Imagen */}
+                <img
+                  src="/images/logopag.jpg"
+                  alt="Logo"
+                  className="w-40 h-auto mr-3"
                 />
+
+                {/* Texto */}
+                <div className="text-center">
+                  <h1 className="poppins-bold uppercase text-black">
+                    !Olvidé mi
+                  </h1>
+                  <h1 className="poppins-bold uppercase text-black">
+                    contraseña¡
+                  </h1>
+                </div>
               </div>
-
-              <div className="mb-3">
-                <button className="poppins-regular green text-white border py-2 w-full rounded-xl mt-5 hover:scale-105 duration-300 hover:bg-emerald-900 hover:text-white">
-                  Enviar correo de recuperación
-                </button>
+              <div className="text-center">
+                <small className="poppins-regular text-black block mt-4 ">
+                  !Tranquilo técnico!
+                </small>
+                <small className="poppins-regular text-black block mb-4 ">
+                  Le ayudamos a recuperar su contraseña
+                </small>
               </div>
-            </form>
-            {Object.keys(mensaje).length > 0 && (
-              <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
-            )}
+              <form onSubmit={handleSubmit}>
+                <div className="mb-1">
+                  <label className="mb-1 block poppins-semibold">
+                    Correo electrónico
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Ingrese su correo electrónico"
+                    className="poppins-regular block w-full rounded-xl border border-gray-300 focus:border-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-700 py-1 px-2 text-black"
+                    name="email"
+                    onChange={handleChange}
+                  />
+                </div>
 
-            <div className="border-black mt-1 text-xs border-b-2 py-2 opacity-20"></div>
+                <div className="mb-3">
+                  <button className="poppins-regular green text-white border py-2 w-full rounded-xl mt-5 hover:scale-105 duration-300 hover:bg-emerald-900 hover:text-white">
+                    Enviar correo de recuperación
+                  </button>
+                </div>
+              </form>
+              {Object.keys(mensaje).length > 0 && (
+                <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
+              )}
 
-            <div className="poppins-regular mt-3 text-sm flex justify-between items-center">
-              <p>¿Recordaste tu contraseña?</p>
-              <Link
-                to="/"
-                className="poppins-regular py-2 px-5 green text-white border rounded-xl hover:scale-110 duration-300 hover:bg-emerald-900 hover:text-white"
-              >
-                Iniciar sesión
-              </Link>
-              {/* <Link
+              <div className="border-black mt-1 text-xs border-b-2 py-2 opacity-20"></div>
+
+              <div className="poppins-regular mt-3 text-sm flex justify-between items-center">
+                <p>¿Recordaste tu contraseña?</p>
+                <Link
+                  to="/"
+                  className="poppins-regular py-2 px-5 green text-white border rounded-xl hover:scale-110 duration-300 hover:bg-emerald-900 hover:text-white"
+                >
+                  Iniciar sesión
+                </Link>
+                {/* <Link
               to="/login"
               className="py-2 px-5 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white"
             >
               Iniciar sesión
             </Link> */}
+              </div>
             </div>
           </div>
         </div>
--- src/paginas/NotFound.jsx
+++ src/paginas/NotFound.jsx
@@ -1,24 +1,37 @@
-import logoDog from '../assets/doglost.jpg'
-import {Link} from 'react-router-dom'
-
+import logoDog from "/images/problem.jpg";
+import { Link } from "react-router-dom";
 
 export const NotFound = () => {
-    return (
-        
-
+  return (
+    <div className="w-full h-screen relative">
+      {/* Fondo */}
+      <div className="absolute inset-0 bg-[url('/images/tecnico.jpg')] bg-no-repeat bg-cover bg-center opacity-70 blur-sm"></div>
+      <div className="absolute inset-0 flex justify-center items-center">
         <div className="flex flex-col items-center justify-center">
+          <img
+            class="object-cover h-80 w-80 rounded-full border-4 border-solid border-black"
+            src={logoDog}
+            alt="image description"
+          />
 
-            <img class="object-cover h-80 w-80 rounded-full border-4 border-solid border-slate-600" src={logoDog} alt="image description"/>
+          <div className="flex flex-col items-center justify-center">
+            <p className="poppins-bold text-3xl md:text-4xl lg:text-5xl text-black mt-12">
+              PÁGINA NO ENCONTRADA
+            </p>
 
-            <div className="flex flex-col items-center justify-center">
-                
-                <p className="text-3xl md:text-4xl lg:text-5xl text-gray-800 mt-12">Page Not Found</p>
-                
-                <p className="md:text-lg lg:text-xl text-gray-600 mt-8">Sorry, the page you are looking for could not be found.</p>
-                
-                <Link to="/login" className="p-3 m-5 w-full text-center  bg-gray-600 text-slate-300  border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white">Login</Link>
+            <p className="poppins-semibold md:text-lg lg:text-xl text-black mt-8">
+              Lo siento... No se ha encontrado la página que buscas.
+            </p>
 
-            </div>
+            <Link
+              to="/"
+              className="poppins-regular p-3 m-5 w-full text-center green text-white  border rounded-xl hover:scale-105 duration-300 border-black hover:bg-emerald-900 hover:text-white"
+            >
+              Volver al inicio de sesión
+            </Link>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
