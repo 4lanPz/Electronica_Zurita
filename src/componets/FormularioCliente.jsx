0f32c1e64ebf15581a1aa23ec87606b104eb923b
Alan <alanstvn420@gmail.com>
2024-05-16 13:54:56 -0500

    registro clientes 1

--- src/componets/FormularioCliente.jsx
+++ src/componets/FormularioCliente.jsx
@@ -1,62 +1,60 @@
-import { useContext, useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Mensaje from "./Alertas/Mensaje";
 
 export const FormularioCliente = ({ cliente }) => {
   const navigate = useNavigate();
+  const [map, setMap] = useState(null);
+  const [marker, setMarker] = useState(null);
   const [mensaje, setMensaje] = useState({});
-  const [form, setform] = useState({
-    nombre: cliente?.nombre ?? "",
-    correo: cliente?.correo ?? "",
-    celular: cliente?.celular ?? "",
-    cedula: cliente?.cedula ?? "",
+  const [form, setForm] = useState({
+    nombre: "", //string
+    correo: "", //email
+    celular: "", //number
+    cedula: "", //number
+    frecuente: "", //booleano
+    direccion: "", //string
   });
 
   const handleChange = (e) => {
-    setform({ ...form, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    const newValue = name === "frecuente" ? value === "true" : value;
+
+    setForm({
+      ...form,
+      [name]: newValue,
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (paciente?._id) {
+    try {
       const token = localStorage.getItem("token");
-      const url = `${import.meta.env.VITE_BACKEND_URL}/cliente/actualizar/${
-        paciente?._id
-      }`;
+      const url = `${import.meta.env.VITE_BACKEND_URL}/cliente/registro`;
       const options = {
         headers: {
-          method: "PUT",
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
       };
-      await axios.put(url, form, options);
-      navigate("/dashboard/listarclientes");
-    } else {
-      try {
-        const token = localStorage.getItem("token");
-        const url = `${import.meta.env.VITE_BACKEND_URL}/cliente/registro`;
-        const options = {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        };
-        await axios.post(url, form, options);
-        setMensaje({
-          respuesta: "cliente registrado con exito y correo enviado",
-          tipo: true,
-        });
-        setTimeout(() => {
-          navigate("/dashboard/listarclientes");
-        }, 30000);
-      } catch (error) {
-        setMensaje({ respuesta: error.response.data.msg, tipo: false });
-        setTimeout(() => {
-          setMensaje({});
-        }, 30000);
-      }
+      await axios.post(url, form, options);
+      setMensaje({
+        respuesta: "Cliente registrado con exito y correo enviado",
+        tipo: true,
+      });
+      setTimeout(() => {
+        navigate("/dashboard/listarclientes");
+      }, 3000);
+    } catch (error) {
+      setMensaje({ respuesta: error.response.data.msg, tipo: false });
+      setTimeout(() => {
+        setMensaje({});
+      }, 30000);
+    } finally {
+      setTimeout(() => {
+        setMensaje({});
+      }, 30000);
     }
   };
 
@@ -118,144 +116,143 @@ export const FormularioCliente = ({ cliente }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {Object.keys(mensaje).length > 0 && (
-        <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
-      )}
-      <div>
-        <label
-          htmlFor="nombre:"
-          className="text-gray-700 uppercase font-bold text-sm"
-        >
-          Nombre de la mascota:{" "}
-        </label>
-        <input
-          id="nombre"
-          type="text"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
-          placeholder="nombre de la mascota"
-          name="nombre"
-          value={form.nombre}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="correo:"
-          className="text-gray-700 uppercase font-bold text-sm"
-        >
-          Email:{" "}
-        </label>
-        <input
-          id="correo"
-          type="email"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
-          placeholder="email del propietario"
-          name="correo"
-          value={form.correo}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="w-1/2 pl-2">
-        <label
-          htmlFor="cedula:"
-          className="poppins-semibold text-black uppercase"
-        >
-          Cédula:
-        </label>
-        <input
-          id="cedula"
-          type="text"
-          inputMode="numberic"
-          className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600 mb-3"
-          placeholder="Cédula del cliente"
-          name="cedula"
-          value={form.cedula}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="celular:"
-          className="text-gray-700 uppercase font-bold text-sm"
-        >
-          Celular:{" "}
-        </label>
-        <input
-          id="celular"
-          type="number"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
-          placeholder="celular del propietario"
-          name="celular"
-          value={form.celular}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="frecuente:"
-          className="poppins-semibold text-black uppercase"
-        >
-          Cliente frecuente
-        </label>
-        <select
-          id="frecuente"
-          className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600 mb-3"
-          name="frecuente"
-          value={form.frecuente}
-          onChange={handleChange}
-        >
-          <option value="">Seleccionar</option>
-          <option value="true">Sí</option>
-          <option value="false">No</option>
-        </select>
-      </div>
-      <div>
-        {/* Campo de entrada de dirección */}
-        <label
-          htmlFor="direccion:"
-          className="poppins-semibold text-black uppercase"
-        >
-          Dirección
-        </label>
-        <input
-          id="direccion"
-          type="text"
-          className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600"
-          placeholder="Dirección"
-          name="direccion"
-          value={form.direccion}
-          onChange={handleChange}
-        />
-      </div>
+    <div className="p-8 w-full flex justify-center">
+      <div className="xl:w-2/3 justify-center items-center">
+        <form onSubmit={handleSubmit}>
+          {Object.keys(mensaje).length > 0 && (
+            <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
+          )}
+          <div className="poppins-regular">
+            <label
+              htmlFor="nombre:"
+              className="poppins-semibold text-black uppercase"
+            >
+              Nombre cliente:
+            </label>
+            <input
+              id="nombre"
+              type="text"
+              className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600 mb-3"
+              placeholder="Nombre y apellido del cliente"
+              name="nombre"
+              value={form.nombre}
+              onChange={handleChange}
+            />
+          </div>
 
-      {/* Contenedor del mapa de Google Maps */}
-      <div id="map" style={{ height: "300px", marginTop: "50px" }}></div>
+          <div className="flex flex-wrap">
+            <div className="w-1/2 pr-2">
+              <label
+                htmlFor="celular:"
+                className="poppins-semibold text-black uppercase"
+              >
+                Teléfono / celular:
+              </label>
+              <input
+                id="celular"
+                type="tel"
+                className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600 mb-3"
+                placeholder="Teléfono / celular del cliente"
+                name="celular"
+                value={form.celular}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-1/2 pl-2">
+              <label
+                htmlFor="cedula:"
+                className="poppins-semibold text-black uppercase"
+              >
+                Cédula:
+              </label>
+              <input
+                id="cedula"
+                type="text"
+                inputMode="numberic"
+                className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600 mb-3"
+                placeholder="Cédula del cliente"
+                name="cedula"
+                value={form.cedula}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="correo:"
+              className="poppins-semibold text-black uppercase"
+            >
+              Correo Electrónico:
+            </label>
+            <input
+              id="correo"
+              type="text"
+              className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600 mb-3"
+              placeholder="Correo electrónico del cliente"
+              name="correo"
+              value={form.correo}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="frecuente:"
+              className="poppins-semibold text-black uppercase"
+            >
+              Cliente frecuente
+            </label>
+            <select
+              id="frecuente"
+              className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600 mb-3"
+              name="frecuente"
+              value={form.frecuente}
+              onChange={handleChange}
+            >
+              <option value="">Seleccionar</option>
+              <option value="true">Sí</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+          <div>
+            {/* Campo de entrada de dirección */}
+            <label
+              htmlFor="direccion:"
+              className="poppins-semibold text-black uppercase"
+            >
+              Dirección
+            </label>
+            <input
+              id="direccion"
+              type="text"
+              className="border-2 rounded-xl w-full p-2 mt-2 placeholder-gray-600"
+              placeholder="Dirección"
+              name="direccion"
+              value={form.direccion}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div className="flex justify-center p-3 mb-5">
-        <div className=" text-center poppins-regular bg-[#5B72C3] green p-2 text-white uppercase rounded-xl hover:bg-[#3D53A0] cursor-pointer transition-all w-1/3">
-          <button
-            type="button"
-            onClick={handleConfirmDireccion}
-            className="btn btn-primary"
-          >
-            Confirmar Dirección
-          </button>
-        </div>
+          {/* Contenedor del mapa de Google Maps */}
+          <div id="map" style={{ height: "300px", marginTop: "50px" }}></div>
+
+          <div className="flex justify-center p-3 mb-5">
+            <div className=" text-center poppins-regular bg-[#5B72C3] green p-2 text-white uppercase rounded-xl hover:bg-[#3D53A0] cursor-pointer transition-all w-1/3">
+              <button
+                type="button"
+                onClick={handleConfirmDireccion}
+                className="btn btn-primary"
+              >
+                Confirmar Dirección
+              </button>
+            </div>
+          </div>
+          <input
+            type="submit"
+            className="poppins-regular bg-[#5B72C3] green w-full p-3 text-white uppercase rounded-xl hover:bg-[#3D53A0] cursor-pointer transition-all"
+            value={"Registrar cliente"}
+          />
+        </form>
       </div>
-      <input
-        type="submit"
-        className="poppins-regular bg-[#5B72C3] green w-full p-3 text-white uppercase rounded-xl hover:bg-[#3D53A0] cursor-pointer transition-all"
-        value={"Registrar cliente"}
-      />
-
-      <input
-        type="submit"
-        className="bg-gray-600 w-full p-3 
-                    text-slate-300 uppercase font-bold rounded-lg 
-                    hover:bg-gray-900 cursor-pointer transition-all"
-        value={paciente?._id ? "Actualizar paciente" : "Registrar paciente"}
-      />
-    </form>
+    </div>
   );
 };
