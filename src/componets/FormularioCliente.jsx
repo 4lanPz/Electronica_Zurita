98759648f224f83c7aff21723ef751c0707cd6c5
Alan <alanstvn420@gmail.com>
2024-05-16 13:48:46 -0500

    registro clientes

--- src/componets/FormularioCliente.jsx
+++ src/componets/FormularioCliente.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Mensaje from "./Alertas/Mensaje";
