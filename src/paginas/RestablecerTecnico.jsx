dddd3c72522521ea5d6b125093511ab883a19a1b
Alan <alanstvn420@gmail.com>
2024-07-10 16:42:25 -0500

    cambios recuperar contra cliente y tecnico, formularios validaciones y animacion carga

--- src/paginas/RestablecerTecnico.jsx
+++ src/paginas/RestablecerTecnico.jsx
@@ -3,37 +3,46 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const RestablecerTecnico = () => {
   const { token } = useParams();
   const navigate = useNavigate();
   const [mensaje, setMensaje] = useState({});
   const [tokenback, setTokenBack] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const [form, setForm] = useState({
+  const initialValues = {
     password: "",
     confirmpassword: "",
-  });
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const validationSchema = Yup.object({
+    password: Yup.string()
+      .min(6, "La contraseña debe tener al menos 6 caracteres")
+      .required("Campo requerido"),
+    confirmpassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Las contraseñas deben coincidir")
+      .required("Campo requerido"),
+  });
+
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    setLoading(true);
     try {
       const url = `${import.meta.env.VITE_BACKEND_URL}/nuevo-password/${token}`;
-      const respuesta = await axios.post(url, form);
-      setForm({});
+      const respuesta = await axios.post(url, values);
+      resetForm();
       setMensaje({ respuesta: respuesta.data.msg, tipo: true });
+      setLoading(false);
       setTimeout(() => {
         navigate("/");
-      }, 60000);
+      }, 6000);
     } catch (error) {
       setMensaje({ respuesta: error.response.data.msg, tipo: false });
+      setLoading(false);
     }
+    setSubmitting(false);
   };
 
   const verifyToken = async () => {
@@ -48,6 +57,7 @@ const RestablecerTecnico = () => {
       setMensaje({ respuesta: error.response.data.msg, tipo: false });
     }
   };
+
   useEffect(() => {
     verifyToken();
   }, []);
@@ -70,7 +80,7 @@ const RestablecerTecnico = () => {
               {/* Texto */}
               <div className="text-center">
                 <h1 className="poppins-bold uppercase text-black">
-                  Recuperar <br/> Contraseña
+                  Recuperar <br /> Contraseña
                 </h1>
               </div>
             </div>
@@ -78,37 +88,57 @@ const RestablecerTecnico = () => {
               <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
             )}
             {tokenback && (
-              <form className="w-full" onSubmit={handleSubmit}>
-                <div className="mb-3 mt-3">
-                  <label className="poppins-semibold">
-                    Ingresa tu nueva contraseña
-                  </label>
-                  <input
-                    type="password"
-                    placeholder="Nueva contraseña"
-                    className="mt-2 mb-3 poppins-regular block w-full rounded-xl border border-gray-300 focus:border-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-700 py-1 px-2 text-black"
-                    value={form.password || ""}
-                    name="password"
-                    onChange={handleChange}
-                  />
-                  <label className="poppins-semibold">
-                    Confirma tu nueva contraseña
-                  </label>
-                  <input
-                    type="password"
-                    placeholder="Repite tu contraseña"
-                    className="mt-2 poppins-regular block w-full rounded-xl border border-gray-300 focus:border-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-700 py-1 px-2 text-black"
-                    value={form.confirmpassword || ""}
-                    name="confirmpassword"
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="mb-3">
-                  <button className="poppins-regular py-2 px-5 w-full mt-3 bg-[#5B72C3] text-white border rounded-xl hover:scale-100 duration-300 hover:bg-[#3D53A0] hover:text-white">
-                    Enviar nueva contraseña
-                  </button>
-                </div>
-              </form>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+              >
+                {({ isSubmitting }) => (
+                  <Form className="w-full">
+                    <div className="mb-3 mt-3">
+                      <label className="poppins-semibold">
+                        Ingresa tu nueva contraseña
+                      </label>
+                      <Field
+                        type="password"
+                        name="password"
+                        placeholder="Nueva contraseña"
+                        className="mt-2 mb-3 poppins-regular block w-full rounded-xl border border-gray-300 focus:border-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-700 py-1 px-2 text-black"
+                      />
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="text-red-500"
+                      />
+                      <label className="poppins-semibold">
+                        Confirma tu nueva contraseña
+                      </label>
+                      <Field
+                        type="password"
+                        name="confirmpassword"
+                        placeholder="Repite tu contraseña"
+                        className="mt-2 poppins-regular block w-full rounded-xl border border-gray-300 focus:border-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-700 py-1 px-2 text-black"
+                      />
+                      <ErrorMessage
+                        name="confirmpassword"
+                        component="div"
+                        className="text-red-500"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <button
+                        type="submit"
+                        className={`poppins-regular py-2 px-5 w-full mt-3 bg-[#5B72C3] text-white border rounded-xl hover:scale-100 duration-300 hover:bg-[#3D53A0] hover:text-white ${
+                          loading ? "animate-pulse" : ""
+                        }`}
+                        disabled={isSubmitting || loading}
+                      >
+                        {loading ? "Cargando..." : "Enviar nueva contraseña"}
+                      </button>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
             )}
           </div>
         </div>
